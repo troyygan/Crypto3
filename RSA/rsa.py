c2d8eb19aa5c9b417f090f5e42229c047bc7b4d1
root <Troy>
2019-11-11 11:41:29 +1300

    Update AES, DES, 3DES, and RSA

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,121 +1,63 @@
-'''
-620031587
-Net-Centric Computing Assignment
-Part A - RSA Encryption
-'''
+from rsa_ob import *
+import commands
+import os
+from datetime import datetime, date, time
 
-import random
 
-class rsa(object):
-'''
-Euclid's algorithm for determining the greatest common divisor
-Use itseration to make it faster for larger integers
-'''
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+@profile
 
-'''
-Euclid's extended algorithm for finding the multiplicative inverse of two numbers
-'''
-def multiplicative_inverse(e, phi):
-    d = 0
-    x1 = 0
-    x2 = 1
-    y1 = 1
-    temp_phi = phi
-    
-    while e > 0:
-        temp1 = temp_phi/e
-        temp2 = temp_phi - temp1 * e
-        temp_phi = e
-        e = temp2
-        
-        x = x2- temp1* x1
-        y = d - temp1 * y1
-        
-        x2 = x1
-        x1 = x
-        d = y1
-        y1 = y
-    
-    if temp_phi == 1:
-        return d + phi
+def func_rsa():
+  print "RSA Encryption-Decryption Started....."
+  before_time_enc = datetime.now()
+  r = RSA()
+  encrypt = r.encrypt
+  decrypt = r.decrypt
+  print "RSA Encrypter/ Decrypter"
 
-'''
-Tests to see if a number is prime.
-'''
-def is_prime(num):
-    if num == 2:
-        return True
-    if num < 2 or num % 2 == 0:
-        return False
-    for n in xrange(3, int(num**0.5)+2, 2):
-        if num % n == 0:
-            return False
-    return True
 
-def generate_keypair(p, q):
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
-    elif p == q:
-        raise ValueError('p and q cannot be equal')
-    #n = pq
-    n = p * q
-    print "value of n is:",n
-    #Phi is the totient of n
-    phi = (p-1) * (q-1)
-    print "Euler's totient value:",phi
+##If you want to make the script interactive you can uncomment the lines below
+#  p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+#  q = int(raw_input("Enter another prime number (Not one you entered above): "))
+  p = 89
+  q = 97
+  print "Using %s and %s for generating keys" % (p, q)
+  print "Generating your public/private keypairs now . . ."
+  public, private = r.generate_keypair(p, q)
+  print "Your public key is ", public ," and your private key is ", private
+#  message = raw_input("Enter a message to encrypt with your private key: ")
+  with open ("testfile.txt", "rb") as file:
+      message = file.read()
+  encrypted_msg = encrypt(private, message)
+  print "Encrypted message is: "
+  print ''.join(map(lambda x: str(x), encrypted_msg))
+  after_time_enc = datetime.now()
+  total_time_enc = after_time_enc - before_time_enc
 
-    #Choose an integer e such that e and phi(n) are coprime
-    e = random.randrange(1, phi)
 
-    #Use Euclid's Algorithm to verify that e and phi(n) are comprime
-    g = gcd(e, phi)
-    while g != 1:
-        e = random.randrange(1, phi)
-        g = gcd(e, phi)
+###DECRYPTIOn
 
-    #Use Extended Euclid's Algorithm to generate the private key
-    d = multiplicative_inverse(e, phi)
-    print "value of e is:",e
-    print "value of d is",d
-    #Return public and private keypair
-    #Public key is (e, n) and private key is (d, n)
-    return ((e, n), (d, n))
+  print "Decryption Started..."
+  before_time_dec = datetime.now()
+  print "Decrypting message with public key ", public ," . . ."
+  print "Decrypted Message:"
+  print decrypt(public, encrypted_msg)
+  after_time_dec = datetime.now()
+  total_time_dec = after_time_dec - before_time_dec
 
-def encrypt(pk, plaintext):
-    #Unpack the key into it's components
-    key, n = pk
-    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
-    cipher = [(ord(char) ** key) % n for char in plaintext]
-    #Return the array of bytes
-    return cipher
+#Timestamps for encryption and decryption
+  print "----ENCRYPTION TIME----"
+  print "Start Time: ", before_time_enc
+  print "End Time: ", after_time_enc
+  print "Total Time of Execution: ", total_time_enc
+  print "\n"
 
-def decrypt(pk, ciphertext):
-    #Unpack the key into its components
-    key, n = pk
-    #Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
-    #Return the array of bytes as a string
-    return ''.join(plain)
-    
+  print "----DECRYPTION TIME----"
+  print "Start Time: ", before_time_dec
+  print "End Time: ", after_time_dec
+  print "Total Time of Execution: ", total_time_dec
+  print "\n"
 
-if __name__ == '__main__':
-    '''
-    Detect if the script is being run directly by the user
-    '''
-    print "RSA Encrypter/ Decrypter"
-    p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
-    q = int(raw_input("Enter another prime number (Not one you entered above): "))
-    print "Generating your public/private keypairs now . . ."
-    public, private = generate_keypair(p, q)
-    print "Your public key is ", public ," and your private key is ", private
-    message = raw_input("Enter a message to encrypt with your private key: ")
-    encrypted_msg = encrypt(private, message)
-    print "Your encrypted message is: "
-    print ''.join(map(lambda x: str(x), encrypted_msg))
-    print "Decrypting message with public key ", public ," . . ."
-    print "Your message is:"
-    print decrypt(public, encrypted_msg)
+
+
+func_rsa()
+
